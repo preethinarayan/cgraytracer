09136c1d0cf5555d1e14f0b82244c9159034eefd
dhrumins <dhrumins@074c0a88-b503-11dd-858c-a3a1ac847323>
2008-11-21 03:34:46 +0000

    new stuff added 10.30 pm Nov 20

--- hw4-predhru/Scene.h
+++ hw4-predhru/Scene.h
@@ -3,47 +3,69 @@
 #include<math.h>
 #include<stdlib.h>
 
+#define WIDTH 600
+#define HEIGHT 400
+
+//void traceScene();
+
 class CameraRay
 {
 private:
-	vec3 origin;
-	vec3 direction;
+	vec3 u,v,w;
+	vec3 eye;
+	vec3 center;
+	vec3 up;
+	float fov;
+
 public:
-	CameraRay()
-	{
-		origin = vec3(0,0,0);
-		direction = vec3(0,0,0);
-	}
-	CameraRay(vec3& _origin,vec3& _direction)
-	{
-		origin = vec3(_origin);
-		direction = vec3(_direction);
-	}
+	CameraRay();
+	CameraRay(vec3& eye, vec3& center, vec3& up, float _fov);
+	vec3 calcRays(int i,int j);
+	vec3 getEye(){return eye;}
+};
+CameraRay *camera;
+
+class Ray
+{
+public:
+	vec3 begin; 
+	vec3 end;
+	Ray();
 };
 
-
-class Plane
+class Scene
 {
 private:
-	vec3 normal;
-	float d;
+
 public:
-	Plane()
-	{
-		normal = vec3(0,0,0);
-		d = 0.0;
-	}
-	Plane(vec3& _normal,float dist)
-	{
-		normal = vec3(_normal);
-		d = dist;
-	}
-	vec3 getNormal()
-	{
-		return normal;
-	}
-	float getd()
-	{
-		return d;
-	}
+Ray ray;
+void generateRays();
+
+
 };
+//
+//class Plane
+//{
+//private:
+//	vec3 normal;
+//	float d;
+//public:
+//	Plane()
+//	{
+//		normal = vec3(0,0,0);
+//		d = 0.0;
+//	}
+//	Plane(vec3& _normal,float dist)
+//	{
+//		normal = vec3(_normal);
+//		d = dist;
+//	}
+//	vec3 getNormal()
+//	{
+//		return normal;
+//	}
+//	float getd()
+//	{
+//		return d;
+//	}
+//};
