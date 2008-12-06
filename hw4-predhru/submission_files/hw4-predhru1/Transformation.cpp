f078ddba70669d0732972de777635a1047732f64
dhrumins <dhrumins@074c0a88-b503-11dd-858c-a3a1ac847323>
2008-12-06 04:05:09 +0000

    transformation

---
+++ hw4-predhru/submission_files/hw4-predhru1/Transformation.cpp
@@ -0,0 +1,135 @@
+#include "Transformation.h"
+
+mat4 IDENTITY(1, 0, 0, 0, 
+			  0, 1, 0, 0, 
+			  0, 0, 1, 0, 
+			  0, 0, 0, 1);
+
+void Transformation::push(mat4 Mat)
+{
+	stack[++top]=new mat4(Mat);
+}
+
+mat4 Transformation::pop()
+{
+	mat4 Mat=*(stack[top]);
+	//deleting from the top of the stack
+	delete stack[top--];
+	//return the current popped element
+	return Mat;
+}
+
+void Transformation::calcTransform()
+{
+	int k;
+	mat4 ret;
+	//assign current transfor to the top of the stack
+	transform = *stack[top];
+	for(k=top-1; k>=0; k--)
+	{
+		//right multiplying each element in the stack to get the current transform
+		transform = (*stack[k])*transform;
+	}
+}
+
+
+bool Transformation::pushMat()
+{
+	if(isStackFull())
+		return false;
+	push(IDENTITY);
+	return true;
+}
+
+bool Transformation::popMat()
+{
+	if(isStackEmpty())
+		return false;
+	pop();
+	calcTransform();
+	return true;
+}
+
+bool Transformation::translate(vec3 trans)
+{/*
+	if( !args )
+		return false;
+
+	for(int i=0; i<3; i++)
+	{
+		if(!args[i])
+			return false;
+	}
+*/
+	//forming the translate matrix
+	mat4 T(	1, 0, 0, 0, 
+			0, 1, 0, 0, 
+			0, 0, 1, 0, 
+			trans.x,trans.y,trans.z,1);
+			//atof(args[0]), atof(args[1]), atof(args[2]), 1);
+
+	//pushing it into the stack
+	push(pop()*T);
+	//getting the current transform from the stack elements.
+	calcTransform();
+
+	return true;
+}
+
+bool Transformation::rotate(vec3 Rotate, float angle)
+{
+	/*
+	if( !args )
+		return false;
+
+	for(int i=0; i<4; i++)
+	{
+		if(!args[i])
+			return false;
+	}
+*/
+	mat4 R=IDENTITY;
+	vec3 r = Rotate;
+	//rotatating the matrix R by 'angle' in the direction
+	R.set_rot(nv_pi*(angle)/180, r);
+	push(pop()*R);
+	calcTransform();
+
+	return true;
+}
+
+bool Transformation::scale(vec3 sc)
+{
+	/*
+	if( !args )
+		return false;
+
+	for(int i=0; i<3; i++)
+	{
+		if(!args[i])
+			return false;
+	}
+*/
+	//compute the scaling matrix
+	mat4 S(	sc.x, 0, 0, 0, 
+			0, sc.y, 0, 0, 
+			0, 0, sc.z, 0, 
+			0, 0, 0, 1);
+	//pushing it on the stack
+	push(pop()*S);
+	//calculating the current transform
+	calcTransform();
+	
+	return true;
+}
+
+Transformation::Transformation(void)
+{
+	top=-1;
+	push(IDENTITY);	
+	transform=IDENTITY;
+}
+
+Transformation::~Transformation(void)
+{
+}
---
+++ hw4-predhru/submission_files/hw4-predhru1/Transformation.h
@@ -0,0 +1,28 @@
+#include "nv/nv_math.h"
+
+#define MAX 12
+
+/* maintain current transform stack */
+class Transformation
+{
+	mat4 *stack[MAX];
+	int top;
+	mat4 transform;
+
+public:
+	Transformation(void);
+	void push(mat4 Mat);
+	mat4 pop();
+	bool isStackEmpty(){return top==-1;};
+	bool isStackFull(){return top==MAX-1;};
+	void calcTransform();
+	mat4 getTransform(){return transform;};
+	bool pushMat();
+	bool popMat();
+
+	bool translate(vec3 trans);
+	bool rotate(vec3 Rotate, float angle);
+	bool scale(vec3 sc);
+public:
+	~Transformation(void);
+};
